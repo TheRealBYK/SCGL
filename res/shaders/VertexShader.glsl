3c88cb03d15553b95cd07767671462ff0a5f6485
TheRealBYK <byknintendo@gmail.com>
2026-08-25 20:53:56 +0300

    Added some technical details.

--- res/shaders/VertexShader.glsl
+++ res/shaders/VertexShader.glsl
@@ -1,3 +1,15 @@
+// Copyright (c) 2026 BYK. All Rights Reserved.
+//
+// This file is part of SCGL.
+//
+// SCGL is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, under version 2 of the License.
+//
+// SCGL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with SCGL. If not, see <https://www.gnu.org/licenses/>.
+//
+// Maintainer: BYK <bykdev@proton.me>
+
 #version 430 core
 
 in layout(location=0) vec3 position;
